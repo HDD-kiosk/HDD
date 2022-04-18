79bf5d156c69ff805cfa0829f08988a3dea885d4
devryyeong <thisisme20@daum.net>
2022-04-18 18:18:16 +0900

    chore: 프로젝트 구조 세팅

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, { Suspense } from 'react';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import Home from '../src/views/Home/Home';
+import Order from '../src/views/Guest/Order/Order';
 
 function App() {
   return (
-    <div className="App">
-      <div>Learn React..!!!!?????~~~~</div>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/order" element={<Order />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
