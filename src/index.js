0ad71ba70ad9272a9a3b2e3054df494d6047cd00
SungminPark1997 <holypp@naver.com>
2022-04-21 17:06:43 +0900

    feat: 관리자 화면 중 네비게이션 바 구현

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import GlobalStyle from './styles/GlobalStyles';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import GlobalStyle from "./styles/GlobalStyles";
+import Navigation from "./Components/Navigation";
+import Colors from "./styles/Colors";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
     <App />
+    <Navigation />
   </React.StrictMode>
 );
 
