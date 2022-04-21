afc7f0abeab2c0e48961f1b5f14994706f28a758
SungminPark1997 <holypp@naver.com>
2022-04-21 17:49:00 +0900

    feat: 네비게이션 바 구현

--- src/index.js
+++ src/index.js
@@ -4,19 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import GlobalStyle from "./styles/GlobalStyles";
-import Navigation from "./Components/Navigation";
-import Colors from "./styles/Colors";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
     <App />
-    <Navigation />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
