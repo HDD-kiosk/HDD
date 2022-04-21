283dbcefc58018dc0ca7b012a5da5d84cd95d8cf
SungminPark1997 <holypp@naver.com>
2022-04-21 18:15:39 +0900

    feat: 네비게이션바 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Suspense } from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
-import Home from "../src/views/Home/Home";
-import Order from "../src/views/Guest/Order/Order";
+import React, { Suspense } from 'react';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import Home from '../src/views/Home/Home';
+import Order from '../src/views/Guest/Order/Order';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { ThemeProvider } from "styled-components";
-import theme from "./styles/theme";
-import GlobalStyle from "./styles/GlobalStyles";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import GlobalStyle from './styles/GlobalStyles';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
