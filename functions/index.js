17e6c59f8d18aa5310eb7459ffdb3fe757206f10
devryyeong <thisisme20@daum.net>
2022-05-23 14:22:02 +0900

    firebase cloud functions 설정

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,13 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const app = express();
+const cors = require('cors')({ origin: true });
+
+app.use(express.json()); // body-parser 설정 
+// app.use("/api", movieApp);
+
+exports.apicall = functions.region("asia-northeast3").https.onRequest(app);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
