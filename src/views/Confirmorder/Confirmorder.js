cd8f64a99a11dc95c7003b07528321e165e92bc9
ghwls9394 <ghwls9394@naver.com>
2022-05-24 01:04:48 +0900

    feat: 기타 기능 구현 및 수정

--- src/views/Confirmorder/Confirmorder.js
+++ src/views/Confirmorder/Confirmorder.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import Hddlogo from "../../img/hddLogo.png";
@@ -25,13 +26,27 @@ const TextArea = styled.div`
   margin-top: 50px;
   border: 1px solid black;
   border-radius: 10px;
+  padding-top: 20px;
+  padding-bottom: 20px;
 `;
 
-const ConfirmText = styled.h3``;
+const TimeArea = styled.div`
+  width: 50%;
+  height: 15%;
+  margin-left: 25%;
+  text-align: center;
+  margin-top: 50px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
+
+const ConfirmText = styled.h3`
+  font-size: 32px;
+`;
 
 const OrdernumberText = styled.h1`
   margin-top: 50px;
-  font-size: 32px;
+  font-size: 50px;
   color: red;
 `;
 
@@ -42,21 +57,34 @@ const Advertise = styled.div`
 const Imgloopy = styled.img.attrs((props) => ({
   src: Loopy,
   wsize: props.size || "100%",
-  hsize: props.size || "100%",
+  hsize: props.size || "280px",
 }))`
   width: ${(props) => props.wsize};
   height: ${(props) => props.hsize};
 `;
 
 const Confirmorder = (props) => {
+  const [time, setTime] = useState(5);
+  const navigate = useNavigate();
+  useEffect(() => {
+    setInterval(() => {
+      setTime(time - 1);
+    }, 1000);
+    if (time == 0) {
+      navigate("/mainguest");
+    }
+  }, [time]);
   return (
     <Container>
       <TopBar>
         <Imglogo></Imglogo>
       </TopBar>
+      <TimeArea>
+        <ConfirmText>{time}초 후 화면이 전환됩니다.</ConfirmText>
+      </TimeArea>
       <TextArea>
         <ConfirmText>주문이 완료되었습니다.</ConfirmText>
-        <OrdernumberText>주문번호 : </OrdernumberText>
+        <OrdernumberText>주문번호 :{props.listNumber} </OrdernumberText>
       </TextArea>
       <Advertise>
         <Imgloopy></Imgloopy>
